7e7afa5a7b3092c08c26725b591a3c5c54e94d5b
Hardcoreshhick <hmelishe8@yandex.ru>
2026-09-17 14:40:21 +0300

    Исправил checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/storage/review/ReviewDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/review/ReviewDbStorage.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.filmorate.storage.review;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.dao.DuplicateKeyException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
--- src/main/java/ru/yandex/practicum/filmorate/storage/review/ReviewStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/review/ReviewStorage.java
@@ -6,14 +6,24 @@ import java.util.List;
 import java.util.Optional;
 
 public interface ReviewStorage {
+
     Review add(Review review);
+
     Review update(Review review);
+
     void delete(Long id);
+
     Optional<Review> findById(Long id);
+
     List<Review> findByFilmId(Long filmId, int count);
+
     List<Review> findAll(int count);
+
     void addLike(Long reviewId, Long userId, boolean isUseful);
+
     void removeLike(Long reviewId, Long userId);
+
     boolean existsByUserAndFilm(Long userId, Long filmId);
+
     boolean existsLike(Long reviewId, Long userId);
 }
