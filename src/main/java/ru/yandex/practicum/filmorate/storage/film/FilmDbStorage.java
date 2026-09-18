9fe40dc0f1c66813bc8f676b2b8f750de385ed45
annkefri <annkefri@yandex.ru>
2026-09-18 16:20:52 +0300

    fix: load directors in getCommonFilms

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmDbStorage.java
@@ -126,6 +126,7 @@ public class FilmDbStorage extends BaseStorage<Film> implements FilmStorage {
     public Collection<Film> getCommonFilms(long userId, long friendId) {
         List<Film> films = findMany(FIND_COMMON_FILMS_QUERY, userId, friendId);
         loadGenresForFilms(films);
+        loadDirectorsForFilms(films);
         return films;
     }
 
